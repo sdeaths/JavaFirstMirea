70aeccd90f03fe95a3467f0c88d5fee936271cef
s_death_s <urvanova03@mail.ru>
2022-12-10 12:11:26 +0300

    15 задание сделано

---
+++ src/ru/mirea/task15/ReadInfo.java
@@ -0,0 +1,18 @@
+package ru.mirea.task15;
+
+import java.io.*;
+
+public class ReadInfo {
+    public static void main(String[] args){
+        try (FileReader reader = new FileReader("C:\\Users\\s_death_s\\IdeaProjects\\JavaFirstMirea\\src\\ru\\mirea\\task15\\info1.txt")){
+            System.out.println("Из файла получен текст: ");
+            int c;
+            while ((c = reader.read()) != -1)
+                System.out.print((char) c);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/ru/mirea/task15/WriteInfo.java
@@ -0,0 +1,19 @@
+package ru.mirea.task15;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class WriteInfo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text;
+
+        try(FileWriter writer = new FileWriter("C:\\Users\\s_death_s\\IdeaProjects\\JavaFirstMirea\\src\\ru\\mirea\\task15\\info1.txt")) {
+            System.out.println("Введите информацию, которую необходимо передать в текстовый файл");
+            text = scanner.nextLine();
+            writer.write(text);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
