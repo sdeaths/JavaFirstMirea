f0f87bc3888eb67150446e0f707f5d26b410df7e
s_death_s <urvanova03@mail.ru>
2022-12-15 13:51:43 +0300

    30, 31 сделано

---
+++ src/ru/mirea/task30/IP.java
@@ -0,0 +1,12 @@
+package ru.mirea.task30;
+
+import java.util.regex.Pattern;
+
+public class IP {
+    static String reg = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)";
+
+    static boolean checkIP(String str)
+    {
+        return Pattern.matches(reg, str);
+    }
+}
